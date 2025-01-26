ef9e75a7f989cec4fa4b78b6f36f70c86e07be81
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-26 15:42:00 -0500

    Initial setup of MeshPortraitGeneratorViewportTabSummoner

---
+++ Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorViewportTabSummoner.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MeshPortraitGeneratorViewportTabSummoner.h"
+#include "MeshPortraitGeneratorEditor.h"
+
+#define LOCTEXT_NAMESPACE "MeshPortraitGeneratorViewportTabSummoner"
+
+const FName MeshPortraitGeneratorViewportTabId = FName(TEXT("MeshPortraitGeneratorViewportTabId"));
+
+FMeshPortraitGeneratorViewportTabSummoner::FMeshPortraitGeneratorViewportTabSummoner(TSharedPtr<FMeshPortraitGeneratorEditor> InEditor)
+	: FWorkflowTabFactory(MeshPortraitGeneratorViewportTabId, InEditor)
+{
+	MeshPortraitGeneratorEditor = InEditor;
+	TabLabel = LOCTEXT("MeshPortraitGeneratorViewportTabLabel", "Viewport");
+	ViewMenuDescription = LOCTEXT("MeshPortraitGenerator_ViewMenu_Desc", "Mesh Portrait Generator Viewport");
+	ViewMenuTooltip = LOCTEXT("MeshPortraitGenerator_Viewmenu_ToolTip", "Show the Viewport");
+}
+
+TSharedRef<SWidget> FMeshPortraitGeneratorViewportTabSummoner::CreateTabBody(const FWorkflowTabSpawnInfo& Info) const
+{
+	// TSharedPtr<FMeshPortraitGeneratorEditor> Editor = MeshPortraitGeneratorEditor.Pin();
+	return SNew(STextBlock).Text(FText::FromString(TEXT("Test")));
+}
+
+FText FMeshPortraitGeneratorViewportTabSummoner::GetTabToolTipText(const FWorkflowTabSpawnInfo& Info) const
+{
+	return FWorkflowTabFactory::GetTabToolTipText(Info);
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorViewportTabSummoner.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WorkflowOrientedApp/WorkflowTabFactory.h"
+
+/**
+ * 
+ */
+
+class FMeshPortraitGeneratorEditor;
+
+class FMeshPortraitGeneratorViewportTabSummoner : public FWorkflowTabFactory
+{
+public:
+	FMeshPortraitGeneratorViewportTabSummoner(TSharedPtr<FMeshPortraitGeneratorEditor> InEditor);
+
+private:
+	TWeakPtr<FMeshPortraitGeneratorEditor> MeshPortraitGeneratorEditor;
+
+public: // FWorkflowTabFactory Interface
+	virtual TSharedRef<SWidget> CreateTabBody(const FWorkflowTabSpawnInfo& Info) const override;
+	virtual FText GetTabToolTipText(const FWorkflowTabSpawnInfo& Info) const override;
+};
