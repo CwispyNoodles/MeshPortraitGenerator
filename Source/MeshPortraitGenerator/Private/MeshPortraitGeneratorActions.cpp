0975e5c12672790a3e2e16f9ba7d686778559f32
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-26 12:39:44 -0500

    initial setup of MeshPortraitGeneratorActions

--- Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorActions.cpp
+++ Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorActions.cpp
@@ -3,6 +3,38 @@
 
 #include "MeshPortraitGeneratorActions.h"
 
-FMeshPortraitGeneratorActions::FMeshPortraitGeneratorActions(EAssetTypeCategories::Type InCategory)
+#include "MeshPortraitGeneratorInstance.h"
+
+#define LOCTEXT_NAMESPACE "MeshPortraitGeneratorActions"
+
+FMeshPortraitGeneratorActions::FMeshPortraitGeneratorActions(EAssetTypeCategories::Type InAssetCategory)
+	: AssetCategory(InAssetCategory)
 {
 }
+
+FText FMeshPortraitGeneratorActions::GetName() const
+{
+	return NSLOCTEXT("AssetTypeActions", "MeshPortraitGeneratorActions", "Mesh Portrait Generator");
+}
+
+FColor FMeshPortraitGeneratorActions::GetTypeColor() const
+{
+	return FColor::Red;
+}
+
+UClass* FMeshPortraitGeneratorActions::GetSupportedClass() const
+{
+	return UMeshPortraitGeneratorInstance::StaticClass();
+}
+
+void FMeshPortraitGeneratorActions::OpenAssetEditor(const TArray<UObject*>& InObjects,
+	TSharedPtr<IToolkitHost> EditWithinLevelEditor)
+{
+	FAssetTypeActions_Base::OpenAssetEditor(InObjects, EditWithinLevelEditor);
+}
+
+uint32 FMeshPortraitGeneratorActions::GetCategories()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
--- Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorActions.h
+++ Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorActions.h
@@ -12,5 +12,16 @@
  */
 class FMeshPortraitGeneratorActions : public FAssetTypeActions_Base
 {
-	FMeshPortraitGeneratorActions(EAssetTypeCategories::Type InCategory);
+public:
+	FMeshPortraitGeneratorActions(EAssetTypeCategories::Type InAssetCategory);
+
+private:
+	EAssetTypeCategories::Type AssetCategory;
+
+public: // FAssetTypeActions_Base Interface
+	virtual FText GetName() const override;
+	virtual FColor GetTypeColor() const override;
+	virtual UClass* GetSupportedClass() const override;
+	virtual void OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<IToolkitHost> EditWithinLevelEditor) override;
+	virtual uint32 GetCategories() override;
 };
