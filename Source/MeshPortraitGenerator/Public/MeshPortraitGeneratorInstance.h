7bacf2352de77c28d4f09e4d016db1750a60d70f
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-26 12:32:25 -0500

    Created MeshPortraitGeneratorInstance

---
+++ Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "MeshPortraitGeneratorInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MESHPORTRAITGENERATOR_API UMeshPortraitGeneratorInstance : public UObject
+{
+	GENERATED_BODY()
+};
