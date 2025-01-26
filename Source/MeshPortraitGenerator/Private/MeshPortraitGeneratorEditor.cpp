6746b5e7f7ff18ba7781c44d882bd9a05a64cea5
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-26 14:39:51 -0500

    Created MeshPortraitGeneratorEditor

---
+++ Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorEditor.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MeshPortraitGeneratorEditor.h"
+
+void FMeshPortraitGeneratorEditor::InitMeshPortraitGeneratorEditor(const EToolkitMode::Type Mode,
+	const TSharedPtr<IToolkitHost>& InitToolkitHost, UObject* InObject)
+{
+}
+
+void FMeshPortraitGeneratorEditor::RegisterTabSpawners(const TSharedRef<FTabManager>& InTabManager)
+{
+	FWorkflowCentricApplication::RegisterTabSpawners(InTabManager);
+}
+
+FName FMeshPortraitGeneratorEditor::GetToolkitFName() const
+{
+}
+
+FText FMeshPortraitGeneratorEditor::GetBaseToolkitName() const
+{
+}
+
+FString FMeshPortraitGeneratorEditor::GetWorldCentricTabPrefix() const
+{
+}
+
+FLinearColor FMeshPortraitGeneratorEditor::GetWorldCentricTabColorScale() const
+{
+}
+
+FString FMeshPortraitGeneratorEditor::GetDocumentationLink() const
+{
+	return FWorkflowCentricApplication::GetDocumentationLink();
+}
+
+void FMeshPortraitGeneratorEditor::OnToolkitHostingStarted(const TSharedRef<IToolkit>& Toolkit)
+{
+	FWorkflowCentricApplication::OnToolkitHostingStarted(Toolkit);
+}
+
+void FMeshPortraitGeneratorEditor::OnToolkitHostingFinished(const TSharedRef<IToolkit>& Toolkit)
+{
+	FWorkflowCentricApplication::OnToolkitHostingFinished(Toolkit);
+}
---
+++ Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorEditor.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "WorkflowOrientedApp/WorkflowCentricApplication.h"
+#include "MeshPortraitGeneratorEditor.generated.h"
+
+/**
+ * 
+ */
+class MESHPORTRAITGENERATOR_API FMeshPortraitGeneratorEditor : public FWorkflowCentricApplication, public FEditorUndoClient, public FNotifyHook
+{
+
+public:
+	void InitMeshPortraitGeneratorEditor(const EToolkitMode::Type Mode, const TSharedPtr<class IToolkitHost>& InitToolkitHost, UObject* InObject);
+public: // FAssetEditorToolkit interface
+	virtual void RegisterTabSpawners(const TSharedRef<FTabManager>& InTabManager) override;
+	virtual FName GetToolkitFName() const override;
+	virtual FText GetBaseToolkitName() const override;
+	virtual FString GetWorldCentricTabPrefix() const override;
+	virtual FLinearColor GetWorldCentricTabColorScale() const override;
+	virtual FString GetDocumentationLink() const override;
+	virtual void OnToolkitHostingStarted(const TSharedRef<class IToolkit>& Toolkit) override;
+	virtual void OnToolkitHostingFinished(const TSharedRef<class IToolkit>& Toolkit) override;
+};
