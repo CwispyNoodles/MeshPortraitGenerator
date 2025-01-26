a9213bc83a7ecdcfb657bb81622142502eb94348
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-26 12:43:59 -0500

    Initial setup of MeshPortraitGeneratorInstanceFactory

--- Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorInstanceFactory.cpp
+++ Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorInstanceFactory.cpp
@@ -2,3 +2,20 @@
 
 
 #include "MeshPortraitGeneratorInstanceFactory.h"
+
+#include "MeshPortraitGeneratorInstance.h"
+
+UMeshPortraitGeneratorInstanceFactory::UMeshPortraitGeneratorInstanceFactory(const FObjectInitializer& InObjectInitializer)
+	: Super(InObjectInitializer)
+{
+	bCreateNew = true;
+	bEditAfterNew = true;
+	SupportedClass = UMeshPortraitGeneratorInstance::StaticClass();
+}
+
+UObject* UMeshPortraitGeneratorInstanceFactory::FactoryCreateNew(UClass* InClass, UObject* InParent, FName InName,
+	EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+{
+	UMeshPortraitGeneratorInstance* NewMeshPortraitGeneratorInstance = NewObject<UMeshPortraitGeneratorInstance>(InParent, InName, Flags);
+	return NewMeshPortraitGeneratorInstance;
+}
--- Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorInstanceFactory.h
+++ Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorInstanceFactory.h
@@ -13,4 +13,10 @@ UCLASS()
 class MESHPORTRAITGENERATOR_API UMeshPortraitGeneratorInstanceFactory : public UFactory
 {
 	GENERATED_BODY()
+	
+public:
+	UMeshPortraitGeneratorInstanceFactory(const FObjectInitializer& InObjectInitializer);
+
+public: // UFactory Interface
+	virtual UObject* FactoryCreateNew(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
 };
