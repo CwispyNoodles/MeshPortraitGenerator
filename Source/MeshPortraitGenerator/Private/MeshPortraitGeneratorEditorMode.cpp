0e4cb3af6398bbab633bd61c95d7c65bc0f85072
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-26 15:46:49 -0500

    Created MeshPortraitGeneratorEditorMode

---
+++ Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorEditorMode.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MeshPortraitGeneratorEditorMode.h"
+
+#include "MeshPortraitGeneratorViewportTabSummoner.h"
+
+FMeshPortraitGeneratorEditorMode::FMeshPortraitGeneratorEditorMode(TSharedPtr<FMeshPortraitGeneratorEditor> InEditor)
+	: FApplicationMode(TEXT("MeshPortraitGeneratorEditorMode"))
+{
+	
+}
+
+void FMeshPortraitGeneratorEditorMode::RegisterTabFactories(TSharedPtr<FTabManager> InTabManager)
+{
+	FApplicationMode::RegisterTabFactories(InTabManager);
+}
+
+void FMeshPortraitGeneratorEditorMode::PreDeactivateMode()
+{
+	FApplicationMode::PreDeactivateMode();
+}
+
+void FMeshPortraitGeneratorEditorMode::PostActivateMode()
+{
+	FApplicationMode::PostActivateMode();
+}
---
+++ Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorEditorMode.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WorkflowOrientedApp/ApplicationMode.h"
+#include "WorkflowOrientedApp/WorkflowTabManager.h"
+
+class FMeshPortraitGeneratorEditor;
+/**
+ * 
+ */
+class FMeshPortraitGeneratorEditorMode : FApplicationMode
+{
+public:
+	FMeshPortraitGeneratorEditorMode(TSharedPtr<FMeshPortraitGeneratorEditor> InEditor);
+
+private:
+	TWeakPtr<FMeshPortraitGeneratorEditor> MeshPortraitGeneratorEditor;
+	FWorkflowAllowedTabSet Tabs;
+
+public: // FApplicationMode Interface
+	virtual void RegisterTabFactories(TSharedPtr<FTabManager> InTabManager) override;
+	virtual void PreDeactivateMode() override;
+	virtual void PostActivateMode() override;
+};
