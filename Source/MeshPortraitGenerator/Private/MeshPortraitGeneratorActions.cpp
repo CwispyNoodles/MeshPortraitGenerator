c4a58c9454eb4121a8f4cf9d03e4cf32c1f9619c
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-26 12:36:17 -0500

    Created MeshPortraitGeneratorActions

---
+++ Source/MeshPortraitGenerator/Private/MeshPortraitGeneratorActions.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MeshPortraitGeneratorActions.h"
+
+FMeshPortraitGeneratorActions::FMeshPortraitGeneratorActions(EAssetTypeCategories::Type InCategory)
+{
+}
---
+++ Source/MeshPortraitGenerator/Public/MeshPortraitGeneratorActions.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetTypeActions_Base.h"
+#include "UObject/Object.h"
+#include "MeshPortraitGeneratorActions.generated.h"
+
+/**
+ * 
+ */
+class FMeshPortraitGeneratorActions : public FAssetTypeActions_Base
+{
+	FMeshPortraitGeneratorActions(EAssetTypeCategories::Type InCategory);
+};
